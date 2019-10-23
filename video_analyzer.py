314df50dfe90db44851de43fe64385d8334e51a8
sean-sullivan1 <46687337+sean-sullivan1@users.noreply.github.com>
2019-10-22 18:49:44 -0700

    Add files via upload
    Remove file filter, display output during processing so you know its working.

--- video_analyzer.py
+++ video_analyzer.py
@@ -22,63 +22,57 @@ jsonList         = []          #Stores JSON output for every file in filteredFil
 
 #Recursively searches for files in the given path (root directory)
 def getListOfFiles(dirName):
-	# create a list of file and sub directories 
-	# names in the given directory 
-	listOfFile = os.listdir(dirName)
-	allFiles = []
-	# Iterate over all the entries
-	for entry in listOfFile:
-		# Create full path
-		fullPath = os.path.join(dirName, entry)
-		# If entry is a directory then get the list of files in this directory 
-		if os.path.isdir(fullPath):
-			allFiles = allFiles + getListOfFiles(fullPath)
-		else:
-			allFiles.append(fullPath)
-	return allFiles
-
-#Filters fileList to only contain file with white listed extensions
-def getFilteredFileList(list):
-	filteredList = []
-	for i in list:
-		fileExtension = i[-3:].upper()
-		for j in extensionWhiteList:
-			if fileExtension == j.upper():
-				filteredList.append(i)
-	return filteredList
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dirName)
+    allFiles = []
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+    return allFiles
 
 #Uses ffprobe to get file info and returns info in JSON format
 def getVideoMetadata(pathToVideo):
-	cmd = "ffprobe.exe -v quiet -print_format json -show_streams -show_format"
-	args = shlex.split(cmd)
-	args.append(pathToVideo)
-	# run the ffprobe process, decode stdout into utf-a and convert to JSON
-	try:
-		ffprobeOutput = subprocess.check_output(args).decode('utf-8')
-		ffprobeOutput = json.loads(ffprobeOutput)
-	except:
-		print("Error, incompatible file format detected at location " + pathToVideo)
-		ffprobeOutput = "ERROR"
-	return ffprobeOutput
+    cmd = "ffprobe.exe -v quiet -print_format json -show_streams -show_format"
+    args = shlex.split(cmd)
+    args.append(pathToVideo)
+    # run the ffprobe process, decode stdout into utf-a and convert to JSON
+    try:
+        ffprobeOutput = subprocess.check_output(args).decode('utf-8')
+        ffprobeOutput = json.loads(ffprobeOutput)
+    except:
+        print("Error, incompatible file format detected at location " + pathToVideo)
+        ffprobeOutput = "ERROR"
+    return ffprobeOutput
 
-def getJsonList(filteredFileList):
-	list = []
-	for index, i in enumerate(filteredFileList):
-		json = getVideoMetadata(i)
-		if (json == "ERROR"):
-			continue
-		list.append(json)
-		print(index)
-	return list
+def getJsonList(fileList):
+    list = []
+    length = len(fileList)
+    for index, i in enumerate(fileList):
+        json = getVideoMetadata(i)
+        if (json == "ERROR"):
+            continue
+        list.append(json)
+        print("File " + str(index) + " of " + str(length), end='\r', flush=True)
+    return list
 
-fileList         = getListOfFiles(path)
-filteredFileList = getFilteredFileList(fileList)
-jsonList         = getJsonList(filteredFileList)
+print("Getting list of all files")
+print()
+fileList = getListOfFiles(path)
+print("Running ffprobe and converting to JSON")
+print()
+jsonList = getJsonList(fileList)
 
 print(jsonList[0])
 
 if False:
-	for i in filteredFileList:
-		print (i)
-		# If wanted to print each line by line
-		print ()
+    for i in filteredFileList:
+        print (i)
+        # If wanted to print each line by line
+        print ()
